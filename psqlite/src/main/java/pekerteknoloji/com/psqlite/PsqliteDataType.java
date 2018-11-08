5e921aff9b65227e33d6a559507aa7f02144805e
halitpeker <halitpeker06@gmail.com>
2018-11-08 14:05:16 +0300

    Table creator

---
+++ psqlite/src/main/java/pekerteknoloji/com/psqlite/PsqliteDataType.java
@@ -0,0 +1,11 @@
+package pekerteknoloji.com.psqlite;
+/*
+ * Halit PEKER
+ */
+
+public class PsqliteDataType {
+    public static String INTEGER="INTEGER";
+    public static String TEXT ="TEXT";
+    public static String REAL="REAL";
+    public static String PRIMARY="PRIMARY KEY AUTOINCREMENT";
+}
